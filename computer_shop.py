0854eb7ed78d5544ac79d5b1580df28f8dd40f59
Tan Duc Mai <tan.duc.work@gmail.com>
2022-04-13 10:29:31 +0930

    Initialise git repository

---
+++ computer_shop.py
@@ -0,0 +1,25 @@
+# TODO Claim this as your own work by modifying the following...
+# File: computer_shop.py 
+# Author: your name...
+# Id: your id... 
+# Description: your program description...
+# This is my own work as defined by the Academic Integrity policy. 
+import abc
+
+
+
+# TODO Write your classes here...
+
+
+
+def main():
+    # TODO Write your main program code here...
+    print("~~ Welcome to the Computer Store ~~")
+    print()
+
+
+
+# WARNING: Do not write any code in global scope
+
+if __name__ == '__main__':
+    main()
