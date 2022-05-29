c3334b9b71c12e1ee703c48cf5fcb810e99235f5
Tan Duc Mai <tan.duc.work@gmail.com>
2022-05-30 01:23:02 +0930

    Apply method name and property to test_driver

--- test_driver.py
+++ test_driver.py
@@ -21,87 +21,87 @@ import pytest
 
 # ---------------------------- Function Definitions ---------------------------
 @pytest.fixture
-def part_list():
-    return computer_shop.CommandPrompt().get_part_list()
+def partlist():
+    return computer_shop.CommandPrompt().partlist
 
 
-def test_len(part_list):
+def test_len(partlist):
     # Safe case
-    assert len(part_list) == 24
+    assert len(partlist) == 24
 
     # Dangerous case
     with pytest.raises(AssertionError):
-        assert len(part_list) == 0
+        assert len(partlist) == 0
 
 
-def test_get_part_using_name(part_list):
+def test_get_part_using_name(partlist):
     # Safe cases
-    assert isinstance(part_list.get_part_using_name('Toshiba P300'),
+    assert isinstance(partlist.get_part_using_name('Toshiba P300'),
                       computer_shop.Storage)
-    assert isinstance(part_list.get_part_using_name('AMD Ryzen 5'),
+    assert isinstance(partlist.get_part_using_name('AMD Ryzen 5'),
                       computer_shop.CPU)
 
     # Dangerous cases
     with pytest.raises(computer_shop.icontract.errors.ViolationError):
-        part_list.get_part_using_name('')
-        part_list.get_part_using_name(2)
+        partlist.get_part_using_name('')
+        partlist.get_part_using_name(2)
 
     with pytest.raises(AssertionError):
-        assert isinstance(part_list.get_part_using_name('Toshiba'),
+        assert isinstance(partlist.get_part_using_name('Toshiba'),
                           computer_shop.Storage)
-        assert isinstance(part_list.get_part_using_name('AMD Ryzen 5'),
+        assert isinstance(partlist.get_part_using_name('AMD Ryzen 5'),
                           computer_shop.Memory)
 
 
-def test_get_part_using_postion(part_list):
+def test_get_part_using_postion(partlist):
     # Safe cases
-    assert isinstance(part_list.get_part_using_position(23),
+    assert isinstance(partlist.get_part_using_position(23),
                       computer_shop.Storage)
-    assert isinstance(part_list.get_part_using_position(2),
+    assert isinstance(partlist.get_part_using_position(2),
                       computer_shop.CPU)
 
     # Dangerous cases
     with pytest.raises(computer_shop.icontract.errors.ViolationError):
-        part_list.get_part_using_position('')
-        part_list.get_part_using_position(2.0)
+        partlist.get_part_using_position('')
+        partlist.get_part_using_position(2.0)
 
     with pytest.raises(AssertionError):
-        assert isinstance(part_list.get_part_using_position(3),
+        assert isinstance(partlist.get_part_using_position(3),
                           computer_shop.GraphicsCard)
-        assert isinstance(part_list.get_part_using_position(25),
+        assert isinstance(partlist.get_part_using_position(25),
                           computer_shop.Memory)
 
 
-def test_remove_part_using_name(part_list):
+def test_remove_part_using_name(partlist):
     # Safe cases
-    part_list.remove_part_using_name('WD Red')
-    assert len(part_list) == 23
+    partlist.remove_part_using_name('WD Red')
+    assert len(partlist) == 23
 
-    part_list.remove_part_using_name('AMD Ryzen 3')
-    assert len(part_list) == 22
+    partlist.remove_part_using_name('AMD Ryzen 3')
+    assert len(partlist) == 22
 
     # Dangerous cases
     with pytest.raises(computer_shop.icontract.errors.ViolationError):
-        part_list.remove_part_using_name('')
+        partlist.remove_part_using_name('')
 
     with pytest.raises(AssertionError):
-        part_list.remove_part_using_name('WD')
-        assert len(part_list) == 21
+        partlist.remove_part_using_name('WD')
+        assert len(partlist) == 21
 
 
-def test_remove_part_using_postion(part_list):
+def test_remove_part_using_postion(partlist):
     # Safe cases
-    part_list.remove_part_using_position(12)
-    assert len(part_list) == 23
+    partlist.remove_part_using_position(12)
+    assert len(partlist) == 23
 
-    part_list.remove_part_using_position(-12)
-    assert len(part_list) == 22
+    partlist.remove_part_using_position(-12)
+    assert len(partlist) == 22
 
     # Dangerous cases
     with pytest.raises(computer_shop.icontract.errors.ViolationError):
-        part_list.remove_part_using_position('')
-        part_list.remove_part_using_position(12.0)
+        partlist.remove_part_using_position('')
+        partlist.remove_part_using_position(12.0)
 
     with pytest.raises(AssertionError):
-        part_list.remove_part_using_position(25)
-        assert len(part_list) == 21
+        partlist.remove_part_using_position(25)
+        assert len(partlist) == 21
